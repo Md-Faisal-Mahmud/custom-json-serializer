094e007a898438904afd8d21ab8921cdeffab0a9
Muhammad <muhammad.installer@gmail.com>
2024-02-15 11:11:51 +0600

    Added custom-joson-serializer sln and project

---
+++ src/JSON Serializer (Custom)/Classes.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSON_Serializer__Custom_
+{
+    public class Course
+    {
+
+        public object Title;
+
+        public List<Topic> Top { get; set; }
+        public Instructor Teacher { get; set; }
+        public List<Topic> Topics { get; set; }
+        public float Fees1 { get; set; }
+        public double Fees2 { get; set; }
+        public decimal Fees3 { get; set; }
+
+        public float Fees4;
+        public double Fees5;
+        public decimal Fees6;
+
+        public bool IsValid1;
+        public bool IsValid2 { get; set; }
+
+        public enum EnumValues
+        {
+            yes = 1,
+            no,
+            okays
+        }
+
+        public EnumValues myEnumYes { get; set; }
+        public EnumValues myEnumOkay;
+
+        public StructData myStruct;
+
+
+        public List<AdmissionTest> Tests { get; set; }
+    }
+
+    public struct StructData
+    {
+        public int x;
+        public int y;
+    }
+
+    public class AdmissionTest
+    {
+        public DateTime StartDateTime { get; set; }
+        public DateTime EndDateTime { get; set; }
+        public double TestFees { get; set; }
+    }
+
+    public class Topic
+    {
+        public sbyte SBYTE1 { get; set; }
+        public sbyte SBYTE2;
+        public byte BYTE1 { get; set; }
+        public byte BYTE2;
+
+        public short short1 { get; set; }
+        public short short2;
+
+
+        public ushort ushort1 { get; set; }
+        public ushort ushort2;
+
+
+        public uint uint1 { get; set; }
+        public uint uint2;
+
+        public float float1 { get; set; }
+        public float float2;
+
+
+        public char char2;
+
+        public char char1 { get; set; }
+
+
+        public string Title { get; set; }
+        public string id;
+        public string Description { get; set; }
+        public List<Session> Sessions { get; set; }
+    }
+
+    public class Session
+    {
+        public int idx;
+        public int DurationInHour { get; set; }
+        public string LearningObjective { get; set; }
+
+    }
+
+    public class Instructor
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public Address PresentAddress { get; set; }
+        public Address PermanentAddress { get; set; }
+        public List<Phone> PhoneNumbers { get; set; }
+    }
+
+    public class Address
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public char State { get; set; }
+    }
+
+    public class Phone
+    {
+        public string Number { get; set; }
+        public string Extension { get; set; }
+        public string CountryCode { get; set; }
+    }
+
+}
---
+++ src/JSON Serializer (Custom)/Program.cs
@@ -0,0 +1,179 @@
+﻿using JSON_Serializer__Custom_;
+
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+
+
+        Course course = new Course();
+
+        //course.Title = "jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj";
+        //course.Title = 111111111;
+        course.Title = 743894.343;
+
+        course.Top = new List<Topic>()
+        {
+            new Topic
+                    {
+                         SBYTE1 = 1,
+                         SBYTE2 = 1,
+
+                         BYTE1 = 100,
+                         BYTE2 = 78,
+
+                         char1 = 'a',
+                         char2 = 'b',
+
+
+
+
+                        Title= "Title456",
+                        id = null,
+                        Description= "Description",
+                        Sessions = new List<Session>
+                        {
+                            new Session
+                            {
+                                DurationInHour = 2,
+                                LearningObjective = null
+                            }
+                        }
+                    }
+            ,
+            new Topic
+                    {
+                         SBYTE1 = -1,
+                         SBYTE2 = -11,
+                        id = null,
+                        Title= "Title 123",
+                        Description= "Description",
+                        Sessions = new List<Session>
+                        {
+                            new Session
+                            {
+                                idx = 1,
+                                DurationInHour = 2,
+                                LearningObjective = null
+                            }
+                        }
+                    }
+        };
+
+
+
+
+
+
+        course.Fees1 = 30000;
+        course.Fees2 = 30000;
+        course.Fees3 = 30000;
+        course.Fees4 = (float)30000.34;
+        course.Fees5 = 30000;
+        course.Fees6 = 30000.343M;
+
+
+        course.IsValid1 = true;
+        course.IsValid2 = false;
+
+
+        //course.myEnumYes = Course.EnumValues.yes;
+        course.myEnumOkay = Course.EnumValues.okays;
+
+        course.myStruct = new StructData()
+        {
+            x = 3,
+            y = 4
+        };
+
+
+        course.Teacher = new Instructor()
+        {
+            Name = "Jalaluddin",
+            Email = "jalalsir.exe@gmail.com",
+            PresentAddress = new Address()
+            {
+                Street = "4/2",
+                City = "Dhaka",
+                Country = "Bangladesh",
+                State = 's'
+            },
+            PermanentAddress = new Address()
+            {
+                Street = "5/2",
+                City = "Mirpur",
+                Country = "Bangladesh"
+            },
+            PhoneNumbers = new List<Phone>
+            {
+
+                        new Phone()
+                        {
+                            Number = "12343435",
+                            Extension = "017",
+                            CountryCode = "+880"
+                        },
+                        new Phone()
+                        {
+                            Number = "12367891",
+                            Extension = null,
+                            CountryCode = "+880"
+                        }
+                    }
+        };
+
+
+
+        course.Topics = new List<Topic>
+                {
+                    new Topic
+                    {
+                        id = null,
+                        Title= "Title",
+                        Description= "Description",
+                        Sessions = new List<Session>
+                        {
+                            new Session
+                            {
+                                idx = 222,
+                                DurationInHour = 2,
+                                LearningObjective = "Reflection"
+                            }
+                        }
+                    }
+                };
+
+
+
+
+
+        course.Tests = new List<AdmissionTest>
+                {
+                    new AdmissionTest
+                    {
+                        TestFees = 100,
+                        StartDateTime = new DateTime(2023,01,25,9,00,00),
+                        EndDateTime= new DateTime(2023,01,25,11,00,00)
+                    },
+                    new AdmissionTest
+                    {
+                        TestFees = 100,
+                        StartDateTime = new DateTime(2023, 1, 25, 9, 0, 0),
+                        EndDateTime = new DateTime(2023, 1, 25, 15, 0, 0)
+                    }
+                };
+
+
+        string json = JsonFormatter.Convert(course);
+        Console.WriteLine(json);
+
+    }
+}
+
+
+
